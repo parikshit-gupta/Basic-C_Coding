4db83fd39de5fa6a92d4692a0f63aaba688cd975
parikshit-gupta <132159727+parikshit-gupta@users.noreply.github.com>
2023-06-07 22:40:18 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+    float e=9.99;
+    printf("%d\n", e);
+
+    int f=9.99;
+    printf("%d", f);
+
+    printf("enter number a: ");
+    scanf("%d", &a);
+    
+    printf("enter number b: ");
+    scanf("%d", &b);
+    
+    printf("sum is: %d", (a+b));
+    
+    return 0;
+}
